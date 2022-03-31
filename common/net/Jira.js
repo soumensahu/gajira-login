4de6269689d7c77ac6719cef110649832a701267
soumensahu <36691213+soumensahu@users.noreply.github.com>
2022-03-31 12:04:52 +0530

    Update Jira.js

--- common/net/Jira.js
+++ common/net/Jira.js
@@ -79,7 +79,7 @@ class Jira {
     }
 
     if (headers.Authorization === undefined) {
-      headers.Authorization = `Basic ${Buffer.from(`${this.email}:${this.token}`).toString('base64')}`
+      headers.Authorization = `Bearer ${Buffer.from(`${this.email}:${this.token}`).toString('base64')}`
     }
 
     // strong check for undefined
